b054659f3119c03ac2542004e6a0673f2432a1a4
Yasmin Correa de Souza <yasmincorrea1235@gmail.com>
2022-03-10 22:19:27 -0300

    003

--- java/src/Vetor.java
+++ java/src/Vetor.java
@@ -2,9 +2,11 @@ import java.util.Arrays;
 
 public class Vetor {
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade){
         elementos = new String[capacidade];
+        tamanho = 0;
     }
 
 	@Override
@@ -12,12 +14,31 @@ public class Vetor {
 		return "Vetor [elementos=" + Arrays.toString(elementos) + "]";
 	}
 	
-	public void adiciona(String elemento) {
+	/*public void adiciona(String elemento) {
 		for(int i=0; i < elementos.length; i++) {
 			if(elementos[i] == null) {
 				elementos[i] = elemento;
 				break;
 			}
 		}
+	}*/
+	
+	@Override
+	public String toString() {
+		
+		StringBuffer s = new StringBuffer();
+		s.append("[");
+		
+		for(int i=0; i < tamanho-1; i++) {
+			s.append(elementos[i]);
+			s.append(",");
+			
+		}
+	if(tamanho > 0) {
+		s.append(elementos[tamanho-1]);
+		
+	}
+	s.append("]");
+	return s.toString();
 	}
 }
